55a23a7df0492fc6310260fcb20f5c12a517fb31
Pavel Popeskul <peterjj91@gmail.com>
2020-11-14 15:22:14 +0200

    Random image for breed

--- routes/dogs.js
+++ routes/dogs.js
@@ -12,4 +12,16 @@ router.get('/random', function (req, res, next) {
   });
 });
 
+router.get('/breed/:breedId', function (req, res, next) {
+  axios
+    .get(`https://dog.ceo/api/breed/${req.params.breedId}/images/random`)
+    .then((image) => {
+      res.render('dogs', { image: image.data.message });
+    })
+    .catch((err) => {
+      const errorMessage = err.response.data.message;
+      res.send(`Breed ${req.params.breedId} - "${errorMessage}"`);
+    });
+});
+
 module.exports = router;
